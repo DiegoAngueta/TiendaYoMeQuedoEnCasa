bd9c293a91ffe5385f64c710619aba937dc3e647
DiegoAngueta <62485232+DiegoAngueta@users.noreply.github.com>
2020-03-21 18:33:10 -0500

    Update form.js

--- js/form.js
+++ js/form.js
@@ -7,8 +7,8 @@ document.querySelector('#submit').addEventListener('click',function(){
     
 
     let url = "https://api.whatsapp.com/send?phone=593987748145&text=*_Diestal Tienda Online_*%0A*Reservas*%0A%0A*¿Cual es tu nombre?*%0A" + 
-    cliente + "%0A*Indica la direccion de tu casa*%0A" + fecha + "%0A*Indica el o los productos que necesites que te llevemos a tu casa*%0A" + hora + 
-    "%0A*Indica el nimero telefonico de contacto*%0A" + barbero ;
+    cliente + "%0A%0A*Indica la direccion de tu casa*%0A" + fecha + "%0A%0A*Indica el o los productos que necesites que te llevemos a tu casa*%0A" + hora + 
+    "%0A%0A*Indica el nimero telefonico de contacto*%0A" + barbero ;
     window.open(url);
 
 });
